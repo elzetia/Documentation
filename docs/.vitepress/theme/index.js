b3b3d5dfa3dc324f5ca5062916466aaf2a4bc638
KadDarem <cressent.h@orange.fr>
2024-04-25 19:47:15 +0200

    From css to scss

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,20 @@
 import DefaultTheme from 'vitepress/theme'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
-import './custom.css'
+import './custom.scss'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primeicons/primeicons.css'
+import Stepper from 'primevue/stepper';
+import StepperPanel from 'primevue/stepperpanel';
+import Breadcrumb from 'primevue/breadcrumb';
+import MyLayout from './MyLayout.vue'
 
 export default {
   extends: DefaultTheme,
+  Layout: MyLayout,
   enhanceApp({ app }) {
     enhanceAppWithTabs(app)
+    app.component('Stepper',Stepper)
+    app.component('StepperPanel',StepperPanel)
+    app.component('Breadcrumb',Breadcrumb)
   }
 }
